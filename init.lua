d82170eabee268cfa124639f82c4cfb89544d83a
rtgnx <adrian@cybulski.cc>
2022-11-11 23:29:51 +0000

    fix nvim startup errors

--- init.lua
+++ init.lua
@@ -6,3 +6,4 @@ util.installPacker()
 local plugins = require('plugins')
 plugins.setup()
 
+-- vim.api.nvim_set_hl('Normal', {ctermbg='none'})
--- lua/mod/lang/go.lua
+++ lua/mod/lang/go.lua
@@ -3,7 +3,7 @@ return {
     'ray-x/go.nvim',
   },
   setup = function()
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
     require('go').setup({
       -- other setups ....
